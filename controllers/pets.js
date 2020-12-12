0e7c09a71a5b0ea9c52a9948272bf27b326525bc
shani24levi <48565585+shani24levi@users.noreply.github.com>
2020-12-12 15:45:29 +0200

    change add pet func

--- controllers/pets.js
+++ controllers/pets.js
@@ -26,7 +26,7 @@ const getPet = async (req, res) => {
     try {
         let getuserId = req._id;
         //dont show user_id
-        petModel.find({ user_id: getuserId }, { user_id: 0})
+        petModel.find({ user_id: getuserId }, { user_id: 0 })
             .limit(5)
             .then(data => {
                 console.log(data);
@@ -51,26 +51,17 @@ const addPet = async (req, res) => {
         if (!valid.error) {
             try {
                 //check if the dog name exixt for id_user
-                // petModel.find({ name: req.body.name ,  user_id: req.body.user_id }, function(err, user) 
-                // {
-                //    if (err)
-                //    {
-                //        res.send(err);
-                //    }
-                //    console.log(user);
-                //    res.json(user);
-
-                // });
-
-                // console.log(finder);   
-                // if(finder){
-                //   console.log(finder);
-                //   res.status(400).json({message:"pet already in system ",code:"duplicate"});
-                // }
-
-                req.body.user_id = getuserId;
-                let data = await petModel.insertMany([req.body]);
-                res.json(data)
+                petModel.find({ name: req.body.name, user_id: getuserId }, async(err, data) => {
+                    if (data.length >= 1) { // 1 meens found a match 
+                        res.status(400).json({ message: "pet name already in system for the user", code: "duplicate" });
+                    }
+                    else {
+                        //add pet
+                        req.body.user_id = getuserId;
+                        let data = await petModel.insertMany([req.body]);
+                        res.json(data);
+                    }
+                });
             }
             catch (err) {
                 res.status(400).json({ message: "Erorr" });
@@ -125,16 +116,16 @@ const deletePet = async (req, res) => {
     try {
         let getuserId = req._id;
         let idDel = req.params.idDel;
-      
-        let checkPetUserToToken = await petModel.findOne({_id:idDel,user_id:getuserId})
+
+        let checkPetUserToToken = await petModel.findOne({ _id: idDel, user_id: getuserId })
         console.log(checkPetUserToToken)
-        if(!checkPetUserToToken){
-          return res.status(400).json({error:"User is not the pets owner,Unauthorized to delete"})
+        if (!checkPetUserToToken) {
+            return res.status(400).json({ error: "User is not the pets owner,Unauthorized to delete" })
         }
-      
-        petModel.deleteOne({_id:idDel},(err, data) => {
-          if (err) { res.status(400).json(err) }
-          res.json(data);
+
+        petModel.deleteOne({ _id: idDel }, (err, data) => {
+            if (err) { res.status(400).json(err) }
+            res.json(data);
         })
 
     }
@@ -150,14 +141,14 @@ const searchPet = async (req, res) => {
     try {
         let searchQ = req.query.q;
         let mySearch = new RegExp(searchQ);
-      
+
         petModel.find({ $or: [{ name: mySearch }, { gender: mySearch }] })
-          .then(data => {
-            res.json(data)
-          })
-          .catch(err => {
-            res.status(400).json({ err })
-          })
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.status(400).json({ err })
+            })
 
     }
     catch (err) {
