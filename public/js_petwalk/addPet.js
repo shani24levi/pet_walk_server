386d9430ad79d63cb9e38172fe244a9c8d7f6041
or kilim <48524233+orkilim@users.noreply.github.com>
2020-12-26 18:00:40 +0200

    some more features

--- public/js_petwalk/addPet.js
+++ public/js_petwalk/addPet.js
@@ -1,11 +1,17 @@
 let newPet={
     name:"",
-    gender:"",
     type:"",
+    gender:"",
     age:0,
     weight:0,
-    hobbies:[String]
+    day_plan:"",
+    day_plan_level:"",
+    activity:"",
+    food:"",
+    hobbies:""
 }
+
+const url=
 
 $(()=>{
     $("#id_name").on("input",()=>{
@@ -25,7 +31,46 @@ $(()=>{
     })
 
     $("#id_weight").on("input",()=>{
-        newPet.weight=$("#id_wight").val()
+        newPet.weight=$("#id_weight").val()
+    })
+
+    $("#id_dayPlan").on("input",()=>{
+        newPet.day_plan=$("#id_dayPlan").val()
+    })
+
+    $("#id_dayPlanLevel").on("input",()=>{
+        newPet.day_plan_level=$("#id_dayPlanLevel").val()
+    })
+
+    $("#id_activityLevel").on("input",()=>{
+        newPet.activity=$("#id_activityLevel").val()
+    })
+
+    $("#id_foodLevel").on("input",()=>{
+        newPet.food=$("#id_foodLevel").val()
+    })
+
+    $("#id_hobbies").on("input",()=>{
+        newPet.hobbies=$("#id_hobbies").val()
+    })
+
+    $("#add_pet_button").on("click",()=>{
+        console.log("sending data")
+        axios({
+            method:'POST',
+            url:"http://localhost:5000/users",
+            data:newPet,
+            headers: {
+                "x-auth-token": localStorage["token"],
+            }
+        })
+        .then(myData=>{
+            window.location.href = "petProfile.html";
+        })
+        .catch(error=>{
+            console.log(newPet);
+            console.log(error.response); 
+        })
     })
 });
 
