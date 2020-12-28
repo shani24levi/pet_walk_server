6a3e206289dcbf1c1d64b8217513a176c226b408
shani24levi <48565585+shani24levi@users.noreply.github.com>
2020-12-28 22:46:44 +0200

    make it better

--- public/js_petwalk/myInfo/appMyInfo.js
+++ public/js_petwalk/myInfo/appMyInfo.js
@@ -2,7 +2,7 @@ import { doApiGet } from "../../services/apiSer.js";
 import { auth } from "../../services/authSer.js";
 import { createPet } from "./petManager.js";
 import { } from '../../plugs/lightBox.js';
-import {setLocalStorage} from '../myPet/appMyPet/js';
+import {setLocalStorage} from '../myPet/appMyPet.js';
 
 
 $(() => {
