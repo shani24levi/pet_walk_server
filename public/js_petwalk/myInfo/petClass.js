e24222dda015cb8a3fa4f48477651c8834bc6b97
shani24levi <48565585+shani24levi@users.noreply.github.com>
2020-12-29 20:56:34 +0200

    make it better

--- public/js_petwalk/myInfo/petClass.js
+++ public/js_petwalk/myInfo/petClass.js
@@ -106,30 +106,30 @@ class PetClass {
             localStorage.setItem(`index`, `${this.i}`);
         })
 
-            //  //one:2:
-            //  let insidDiv22 = $(`<div class='row p-3 justify-content-between btn_section' style='box-shadow:none'></div>`)
-            //  $(secDiv).append(insidDiv22);
-     
-            //  $(insidDiv22).append(`<div class="col-8 align-self-center p-2">
-            //      <h4><strong>Today's Plans</strong></h4>
-            //      <h5>${this.complitActivity} tasks completed</h5>
-            //  </div>`)
-     
-            //  let pricentDiv22 = $("<div class='col-4 align-self-center text-center demo'></div>");
-            //  $(insidDiv22).append(pricentDiv22);
-     
-            //  //pricent btn 
-            //  let btnPricent22 = $(` 
-            //  <div id='data-light' data-id=${this.id} data-this='currDayPlanLevel' data-name=${this.name} data-src=${this.img} data-alt=${this.i} class="demo-1 align-self-center text-center" data-percent="${this.currActivityLevel}"></div>
-            //  `);
-            //  $(pricentDiv22).append(btnPricent22);
-     
-            //  $(btnPricent22).on("click", () => {
-            //      localStorage.setItem(`petName`, `${this.name}`);
-            //      localStorage.setItem(`petId`, `${this.id}`);
-            //      localStorage.setItem(`update`, `currActivityLevel`);
-            //      localStorage.setItem(`index`, `${this.i}`);
-            //  })
+        //  //one:2:
+        //  let insidDiv22 = $(`<div class='row p-3 justify-content-between btn_section' style='box-shadow:none'></div>`)
+        //  $(secDiv).append(insidDiv22);
+
+        //  $(insidDiv22).append(`<div class="col-8 align-self-center p-2">
+        //      <h4><strong>Today's Plans</strong></h4>
+        //      <h5>${this.complitActivity} tasks completed</h5>
+        //  </div>`)
+
+        //  let pricentDiv22 = $("<div class='col-4 align-self-center text-center demo'></div>");
+        //  $(insidDiv22).append(pricentDiv22);
+
+        //  //pricent btn 
+        //  let btnPricent22 = $(` 
+        //  <div id='data-light' data-id=${this.id} data-this='currDayPlanLevel' data-name=${this.name} data-src=${this.img} data-alt=${this.i} class="demo-1 align-self-center text-center" data-percent="${this.currActivityLevel}"></div>
+        //  `);
+        //  $(pricentDiv22).append(btnPricent22);
+
+        //  $(btnPricent22).on("click", () => {
+        //      localStorage.setItem(`petName`, `${this.name}`);
+        //      localStorage.setItem(`petId`, `${this.id}`);
+        //      localStorage.setItem(`update`, `currActivityLevel`);
+        //      localStorage.setItem(`index`, `${this.i}`);
+        //  })
 
 
 
@@ -205,7 +205,35 @@ class PetClass {
         $(walkDiv).append(walkBtn);
 
         $(walkBtn).on("click", () => {
-            console.log('upate walk');
+            Swal.mixin({
+                input: 'text',
+                confirmButtonText: 'Next &rarr;',
+                showCancelButton: true,
+                progressSteps: ['1', '2', '3']
+            }).queue([
+                {
+                    title: 'Where we play?',
+                    text: 'Garden, park, house, with friends'
+                },
+                'Playing time',
+                'Energy level'
+            ]).then((result) => {
+                console.log(result.value);
+                if (result.value) {
+                    const answers = JSON.stringify(result.value)
+                    Swal.fire({
+                        title: 'All done!',
+                        html: `
+                    Your Day Play:
+                    <pre><code>${answers}</code></pre>
+                  `,
+                        confirmButtonText: 'Lovely!'
+                    })
+                }
+            })
+
+            //update activity level:
+            //updatMyInfo(result.value);
         })
 
         // function set in pet_walk_app in Plangs folder-> jquery.circlechart.js
@@ -216,6 +244,7 @@ class PetClass {
 
         //lightbox
         $(document).lightBox();
+        $('.demo-2').lightBox();
 
 
     }
