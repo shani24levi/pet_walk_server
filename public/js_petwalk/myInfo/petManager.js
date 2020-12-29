3e34d2323fccb9ad3304839897f0a70efae185c6
shani24levi <48565585+shani24levi@users.noreply.github.com>
2020-12-29 23:14:59 +0200

    make it better

--- public/js_petwalk/myInfo/petManager.js
+++ public/js_petwalk/myInfo/petManager.js
@@ -7,10 +7,3 @@ export const createPet = (_arr, i, dataLength) => {
   let pet = new PetClass("main .my_pets",_arr[i].img ,_arr[i].type ,_arr[i].name ,_arr[i].age, _arr[i].dayPlan ,_arr[i].activityLevel, _arr[i].foodLevel,_arr[i].dayPlanLevel,_arr[i].currDayPlanLevel,_arr[i].currActivityLevel,_arr[i].currFoodLevel, i, _arr, dataLength, _arr[i]._id);
   pet.render();
 }
-
-export const createPet2 = (_arr, i, dataLength) => {
-  $("main .my_pets").html("");
-  console.log(_arr[i].img);
-  $("main .my_pets").html(_arr[i].img);
-
-}
